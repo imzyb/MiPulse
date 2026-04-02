2c9494d3db642f501ae6db6e59fabe688b3bb90c
Sinemp <103983721+Sinemp@users.noreply.github.com>
2026-04-02 03:28:31 +0000

    revert: remove database migration files as not needed for new users
    - remove migration_add_network_monitor.sql
    - remove migrate_db.sh
    - remove CLOUDFLARE_PAGES_DEPLOY.md
    - new users don't need migration, schema.sql already includes all columns

--- migration_add_network_monitor.sql
+++
@@ -1,9 +0,0 @@
--- Database Migration: Add network_monitor_enabled column to vps_nodes table
--- This migration adds the network_monitor_enabled column that was added to the schema
--- but may be missing from existing databases
-
--- Add network_monitor_enabled column to vps_nodes table if it doesn't exist
-ALTER TABLE vps_nodes ADD COLUMN network_monitor_enabled INTEGER DEFAULT 1;
-
--- Update existing records to have network_monitor_enabled = 1 (enabled by default)
-UPDATE vps_nodes SET network_monitor_enabled = 1 WHERE network_monitor_enabled IS NULL;
